ee5b8f3e4605b463edb5271a077164cd1dba3a53
xumaozeng <15611895341@163.com>
2021-03-08 22:45:57 +0800

    ts基础类型和对象类型

---
+++ demo-02.ts
@@ -0,0 +1,11 @@
+interface Xiaojiejie {
+  name: string;
+  age: number;
+}
+
+const xiaohong: Xiaojiejie = {
+  name: "小红",
+  age: 18
+};
+
+console.log(xiaohong);
---
+++ demo-03.ts
@@ -0,0 +1,36 @@
+/**
+ * 基础静态类型
+ * null
+ * undefined
+ * symbol
+ * boolean
+ * void
+ */
+const count: number = 918;
+const myName: string = "xiaoxu";
+
+/**
+ * 对象类型
+ * 数组
+ * 类
+ * 函数
+ */
+
+const xiaojiejie: {
+  name: string;
+  age: number;
+} = {
+  name: "大脚",
+  age: 18
+};
+
+console.log(xiaojiejie.name);
+
+const xiaojiejies: string[] = ["谢大脚", "1234"];
+
+class Person {}
+const dajiao: Person = new Person();
+
+const jianXiaojiejie: () => string = () => {
+  return "111";
+};
